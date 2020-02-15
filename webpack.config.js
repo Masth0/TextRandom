2063bec91a4171ea5cff0b5dccb9b897202e9e70
Masth0 <dev.mastho@gmail.com>
2020-02-15 21:14:40 +0100

    Config, Typescript.

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const devMode = process.env.NODE_ENV === 'development';
+
+let config = {
+  name: 'Assets',
+  context: path.resolve('./src'),
+  mode: devMode ? 'development' : 'production',
+  watch: devMode,
+  devServer: {
+    overlay: true,
+    stats: 'errors-only',
+    // CORS
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  },
+  performance: {
+    hints: devMode ? false : 'warning',
+  },
+  entry: {
+    textrandom: './Textrandom.ts'
+  },
+  output: {
+    filename: devMode ? '[name].js' : '[name].min.js',
+    chunkFilename: '[name].bundle.js',
+    path: __dirname + '/dist',
+    library: undefined,
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js" ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader','ts-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: []
+};
+
+module.exports = config;
